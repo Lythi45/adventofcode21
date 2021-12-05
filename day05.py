d55bbd0cb7d183802f6c7d7cf8315a9b301fcd98
Sabine Wolf <scala.bine@gmail.com>
2021-12-05 16:01:36 +0100

    day05

--- day05.py
+++ day05.py
@@ -7,26 +7,17 @@ for modus in range(2):
             if y1>y2:
                 y1,y2=y2,y1
             for y in range(y1,y2+1):
-                if (x1,y) in vents:
-                    vents[(x1,y)]+=1
-                else:
-                    vents[(x1,y)]=1
+                vents[(x1,y)]=vents.get((x1,y),0)+1
         elif y1==y2:
             if x1>x2:
                 x1,x2=x2,x1
             for x in range(x1,x2+1):
-                if (x,y1) in vents:
-                    vents[(x,y1)]+=1
-                else:
-                    vents[(x,y1)]=1
+                vents[(x,y1)]=vents.get((x,y1),0)+1
         elif modus==1:
             rx=[1,-1][x1>x2]
             ry=[1,-1][y1>y2]
             for x in range(x1,x2+rx,rx):
                 y=y1+((x-x1)*rx)*ry
-                if (x,y) in vents:
-                    vents[(x,y)]+=1
-                else:
-                    vents[(x,y)]=1
+                vents[(x,y)]=vents.get((x,y),0)+1
 
     print(sum(map(lambda x:x>1,vents.values())))
