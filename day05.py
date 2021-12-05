95fb8bd8b4d527c6308c20f0e1d32721b4065f65
Sabine Wolf <scala.bine@gmail.com>
2021-12-05 23:07:41 +0100

    day05

--- day05.py
+++ day05.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-for modus in range(2):
+for part in range(2):
     vents=defaultdict(int)
     for line in open("input05.txt","r").readlines():
         (x1,y1),(x2,y2)=map(lambda x:map(int,x.split(",")),line.split(" -> "))
@@ -12,7 +12,7 @@ for modus in range(2):
             rx=[1,-1][x1>x2]
             for x in range(x1,x2+rx,rx):
                 vents[(x,y1)]+=1
-        elif modus==1:
+        elif part==1:
             rx=[1,-1][x1>x2]
             ry=[1,-1][y1>y2]
             for x in range(x1,x2+rx,rx):
@@ -24,24 +24,24 @@ for modus in range(2):
 
 #shorter using ideas from my son
 
-for modus in range(2):
+for part in range(2):
     vents=defaultdict(int)
     for line in open("input05.txt","r").readlines():
         (x1,y1),(x2,y2)=map(lambda x:map(int,x.split(",")),line.split(" -> "))
         rx=[0,-1,1][(x1>x2)+2*(x2>x1)]
         ry=[0,-1,1][(y1>y2)+2*(y2>y1)]
-        if modus==1 or x1==x2 or y1==y2:
+        if part==1 or x1==x2 or y1==y2:
             for l in range(max(abs(x2-x1),abs(y2-y1))+1):
                 vents[(x1+l*rx,y1+l*ry)]+=1
 
     print(sum(map(lambda x:x>1,vents.values())))
 
-for modus in range(2):
+for part in range(2):
     vents=defaultdict(int)
     for line in open("input05.txt","r").readlines():
         (x1,y1),(x2,y2)=map(lambda x:map(int,x.split(",")),line.split(" -> "))
         le=max(abs(x2-x1),abs(y2-y1))
-        if modus==1 or x1==x2 or y1==y2:
+        if part==1 or x1==x2 or y1==y2:
             for l in range(le+1):
                 vents[(x1+l*(x2-x1)//le,y1+l*(y2-y1)//le)]+=1
 
