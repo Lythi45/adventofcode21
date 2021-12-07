38be63e5f3937b1c494aac1df072d4c7ac785cf1
Sabine Wolf <scala.bine@gmail.com>
2021-12-07 09:08:52 +0100

    day07

--- day07.py
+++ day07.py
@@ -1,12 +1,13 @@
 crabs=sorted(map(int,open("input07.txt","r").read().split(",")))
 
-print(sum([abs(crab-crabs[len(crabs)//2]) for crab in crabs]))
+print(sum([abs(crab-crabs[len(crabs)//2]) for crab in crabs])) #median is the best position 
 
 def fuel(dist):
     return dist*(dist+1)//2
 
 mid=int(sum(crabs)/len(crabs))
 
+#one position around the mean position is the best position
 print(min([sum([fuel(abs(crab-mid)) for crab in crabs]),  
            sum([fuel(abs(crab-mid-1)) for crab in crabs])] ))
 
